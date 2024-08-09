2bbf4e67e8dfe54249fa67a6ba6c1cb81481a28a
양세윤 <dosy13@naver.com>
2024-08-09 19:44:14 +0900

    * DB 스키마 생성

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,6 @@
+const User = require('../models/User');
+const Uesr = require('../models/User');
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
@@ -6,11 +9,22 @@ module.exports.login_get = (req, res) => {
     res.render('login');
 }
 
-module.exports.signup_post = (req, res) => {
-    res.send('new signup');
+module.exports.signup_post = async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.create({email, password}); // Promise 반환
+        console.log('user: ', user);
+        res
+            .status(201)
+            .json(user);
+    } catch (error) {
+        console.log('** error: ', error);
+        res.status(400).send('erorr, user not created');
+    }
 }
 
-module.exports.login_post = (req, res) => {
+module.exports.login_post = async (req, res) => {
     const {email, password} = req.body;
 
     console.log('email: ', email, 'password: ', password);
