a482c86486c8f3a72a3721763381712963a2c18d
양세윤 <dosy13@naver.com>
2024-08-09 21:51:13 +0900

    비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     email: {
@@ -14,6 +15,20 @@ const userSchema = new mongoose.Schema({
         required: [true, '비밀번호를 입력해 주세요.'],
         minLength: [6, '6글자 이상의 비밀번호를 입력해 주세요.'],
     }
+});
+
+// db에서 값을 저장한 뒤 실행됨
+userSchema.post('save', function(doc, next){
+    console.log('new user was created & saved', doc);
+    next();
+});
+
+// db에서 값을 저장하기 전에 실행됨
+userSchema.pre('save', async function(next){
+    // 암호화
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 const User = mongoose.model('user', userSchema);
